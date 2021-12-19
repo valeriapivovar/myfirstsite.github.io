1107491e0f43664b3742f1e1e08b0d7d7105046c
Valeria <pivovarvaleria0906@gmail.com>
2021-12-19 22:38:39 +0200

    Updates

---
+++ index.f16fa936.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector("#like");e.addEventListener("click",(()=>e.style.color="red"));const t=document.querySelector("#date"),n=new Date;t.innerHTML=n.getFullYear()}();
+//# sourceMappingURL=index.f16fa936.js.map
